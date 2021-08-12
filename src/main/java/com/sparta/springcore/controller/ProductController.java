5b7e67822ad0af2d45b3f61240e3fab93bf3d593
Lacram <sky114477@naver.com>
2021-08-12 18:07:30 +0900

    1주차 진짜 끝

--- src/main/java/com/sparta/springcore/controller/ProductController.java
+++ src/main/java/com/sparta/springcore/controller/ProductController.java
@@ -1,9 +1,12 @@
-package com.sparta.springcore;
+package com.sparta.springcore.controller;
 
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.dto.ProductMypriceRequestDto;
+import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLException;
 import java.util.List;
 
 @RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
@@ -12,29 +15,23 @@ public class ProductController {
     // 멤버 변수 선언
     private final ProductService productService;
 
-    // 생성자: ProductController() 가 생성될 때 호출됨
-    public ProductController() {
-        // 멤버 변수 생성
-        productService = new ProductService();
-    }
-
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
-    public List<Product> getProducts() throws SQLException {
+    public List<Product> getProducts() {
         // 응답 보내기
         return productService.getProducts();
     }
 
     // 신규 상품 등록
     @PostMapping("/api/products")
-    public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
         // 응답 보내기
         return productService.createProduct(requestDto);
     }
 
     // 설정 가격 변경
     @PutMapping("/api/products/{id}")
-    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
         Product product = productService.updateProduct(id, requestDto);
         return product.getId();
     }
